f4409e3b43ca19610ba82437e11368edf18b25e9
Albert Bitreyeson <albertb@infodata.ca>
2020-06-17 10:29:43 -0400

    corrections

--- src/components/Loader.vue
+++ src/components/Loader.vue
@@ -91,7 +91,7 @@ export default {
                 name: "Sofy"
               },
               {
-                name: "Upixelated"
+                name: "Unpixelated"
               },
             ]
           },
